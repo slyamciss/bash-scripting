86927adeefd91fd003340ac21e6a509e8ddfd7a4
slyamciss <slyamciss@gmail.com>
2022-08-01 13:40:32 -0400

    added a script to print color on screen

--- 02-print.sh
+++ 02-print.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 echo Hello world
+## When we print sometimes, we print for errors so to grab attention of user, it is better to pint in color
+
+
+## print in color
+#Syntax : echo -e "\e[COLmMESS\e[0n"
+ # -e option to enable esc seq \e
+ # \e - to enable color
+ # [COLm - COL is color number
+ # \e[0n - Disable the color
+
+ ## COL Name                        Col Code
+ # Red                              31
+ # Green                            32
+ # Yellow                           33
+ # Blue                             34
+ # Magenta                          35
+ # Cyan                             36
+
+ echo -e "\e[31mRED\e[32mGREEN\e[33mYELLOW\e[34mBLUE\e[35mMAGENTA\e[36mCYAN\e[0n"

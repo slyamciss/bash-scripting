cf854862573d20c4f503b38ce294d56afd7b54ae
slyamciss <slyamciss@gmail.com>
2022-08-01 16:22:50 -0400

    roboshop condition 1

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-bash components/$1.sh
+if [ -f components/"$1".sh ]; then
+  bash components/"$1".sh
+else
+  echo -e "\e[1;31mInvalid Input\e[0m"
+  echo -e "\e[1;33mAvailable Inputs - frontend|mongodb|catalogue|redis|user|cart|mysql|shipping|payment|rabbitmq|dispatch\e[0m"
+  exit 1
+fi

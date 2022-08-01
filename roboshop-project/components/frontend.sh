6f94d83e3ca94773cf3bc5ac90ddf9ec0a106341
slyamciss <slyamciss@gmail.com>
2022-08-01 17:04:55 -0400

    roboshop project frontend setup

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,8 +1,8 @@
-
-rm -f /tmp/roboshop.log
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
 
 echo "Installing NGINX"
-yum install nginx -y >>/tmp/roboshop.log
+yum install nginx -y &>>LOG_FILE
 
 echo "Download Frontend Content"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>/tmp/roboshop.log
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>LOG_FILE

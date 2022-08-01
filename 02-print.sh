f3eca4e9feeec9300bd1f0e90fda9602323d6361
slyamciss <slyamciss@gmail.com>
2022-08-01 13:45:24 -0400

    added a script to print bold color on screen

--- 02-print.sh
+++ 02-print.sh
@@ -8,6 +8,7 @@ echo Hello world
  # -e option to enable esc seq \e
  # \e - to enable color
  # [COLm - COL is color number
+ # MESS - message that will have the color on it
  # \e[0n - Disable the color
 
  ## COL Name                        Col Code
@@ -19,3 +20,7 @@ echo Hello world
  # Cyan                             36
 
  echo -e "\e[31mRED\e[32mGREEN\e[33mYELLOW\e[34mBLUE\e[35mMAGENTA\e[36mCYAN\e[0n"
+
+  ## same colors printed with more brightness with BOLD option
+
+  echo -e "\e[1;31mRED\e[1;32mGREEN\e[1;33mYELLOW\e[1;34mBLUE\e[1;35mMAGENTA\e[1;36mCYAN\e[0n"

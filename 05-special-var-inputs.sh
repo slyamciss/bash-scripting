69610604b46627e5ce8ca7ef9978410c69e85c08
slyamciss <slyamciss@gmail.com>
2022-08-01 15:27:39 -0400

    special variable

---
+++ 05-special-var-inputs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo $0   #Script Name
+echo $1   #First Argument
+echo $2   #Second Argument
+echo $#   #Number of Argument
+echo $*   #All Arguments
+# shellcheck disable=SC2068
+echo $@   #All Argument

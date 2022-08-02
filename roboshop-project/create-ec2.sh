39b948fa6cb1472ceea179a1a9ba11bb01590548
slyamciss <slyamciss@gmail.com>
2022-08-02 17:07:01 -0400

    roboshop project ec2 creation setup

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -29,7 +29,7 @@ INSTANCE_CREATE() {
       exit
     fi
     # Creating Instance
-    aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"  --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}"  &>>$LOG
+    aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"  --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}"  &>>$LOG
     echo -e "\e[1m Instance Created\e[0m"
   else
     echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"

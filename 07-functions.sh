41b2cfa22d1d0f1e65a6825003f869e92219e474
slyamciss <slyamciss@gmail.com>
2022-08-02 17:46:18 -0400

    roboshop project function creation setup

--- 07-functions.sh
+++ 07-functions.sh
@@ -32,5 +32,7 @@ echo $?
 
 # 1. When you declare a variable in main program , then function can access it and modify it
 # 2. When you declare a variable in function , then you can overwrite it in main program
-# 3. Function will not access special variable inputs given to script, Only main program can access them. Meaning special variables for inputs are different for function from main program
-# 4. Cases we need to come back immediately from function to main program then in that case we use return command. return command is like exit command, where exit exits the script where as return with exits the function.
+# 3. Function will not access special variable inputs given to script, Only main program can access them.
+# Meaning special variables for inputs are different for function from main program
+# 4. Cases we need to come back immediately from function to main program then in that case we use return command.
+# return command is like exit command, where exit exits the script where as return with exits the function.

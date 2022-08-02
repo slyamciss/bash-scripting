607ecdb86efd0055392c79b06c35026d20ec1afe
slyamciss <slyamciss@gmail.com>
2022-08-02 12:58:51 -0400

    roboshop project frontend setup

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -12,5 +12,17 @@ echo "Clean old Content"
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 
 echo "Extract Frontend Content"
-cd /usr/share/nginx/html/
+cd /tmp
 unzip /tmp/frontend.zip &>>$LOG_FILE
+
+echo "Copy Extracted Content to Nginx Path"
+cp -r frontend-main/static/* /usr/share/nginx/html/ &>>$LOG_FILE
+
+echo "Copy Nginx RoboShop Config"
+cp frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+
+echo "Start Nginx Service"
+systemctl enable nginx &>>$LOG_FILE
+systemctl restart nginx  &>>$LOG_FILE
+
+
